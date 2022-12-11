68cd1b415f03dda136894b1bf6788a461093543e
mark <imadeveloper23@gmail.com>
2022-12-11 10:03:56 +0800

    change vite hmr host to forge server ip address

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const host = '188.166.230.123';
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -10,11 +12,11 @@ export default defineConfig({
     ],
     server: {
         // host: '0.0.0.0',
-        // host: true,
-        host: '188.166.230.123',
-        // hmr: {
-        //     // host: 'localhost',
-        //     host: '188.166.230.123',
-        // }
+        host,
+        // host: '188.166.230.123',
+        hmr: {
+            // host: 'localhost',
+            host,
+        }
     }
 });
