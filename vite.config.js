067825c88f7ad0b390cafe91750e21304d6ae048
mark <imadeveloper23@gmail.com>
2022-12-11 09:49:51 +0800

    change vite hmr host to forge server ip address

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ export default defineConfig({
         }),
     ],
     server: {
-        // host: '0.0.0.0',
+        host: '0.0.0.0',
         // host: true,
-        host: '188.166.230.123',
+        // host: '188.166.230.123',
         hmr: {
-            host: 'localhost',
-            // host: '188.166.230.123',
+            // host: 'localhost',
+            host: '188.166.230.123',
         }
     }
 });
