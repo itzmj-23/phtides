c613e3ed4b0913a01f45b8978a87edce3d0cf941
mark <imadeveloper23@gmail.com>
2023-02-04 10:13:58 +0800

    trying to fix error in datepicker in prod

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import inject from '@rollup/plugin-inject';
+import {esbuildCommonjs} from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
     plugins: [
@@ -19,6 +20,13 @@ export default defineConfig({
             $: "jquery"
         })
     ],
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                esbuildCommonjs(['bootstrap-datepicker'])
+            ]
+        }
+    },
     server: {
         host: '0.0.0.0',
         // host,
